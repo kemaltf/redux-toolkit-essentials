5ac1803694eb09e3300bb8a394234dabdcc58c37
i am just learner <fikri2701@gmail.com>
2024-01-31 16:44:53 +0000

    Adding new posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { AddPostForm } from "./features/posts/AddPostForm";
 import { PostsList } from "./features/posts/PostList";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      <AddPostForm />
       <PostsList />
     </div>
   );
