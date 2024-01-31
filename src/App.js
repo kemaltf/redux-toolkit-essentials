7dd38cf0b50f757f862e36539da78f2b3fa3b71a
i am just learner <fikri2701@gmail.com>
2024-01-31 17:17:35 +0000

    Feat/Showing single post 1. creating a single post page 2. Adding the single post route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import { AddPostForm } from "./features/posts/AddPostForm";
 import { PostsList } from "./features/posts/PostList";
+import { SinglePostPage } from "./features/posts/SinglePostPage";
+import { Navbar } from "./features/navbar/Navbar";
+
 import "./styles.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <AddPostForm />
-      <PostsList />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
