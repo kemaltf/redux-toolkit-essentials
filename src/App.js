dafe4cfc57f9565ba50749a6b878544a3ccd868c
i am just learner <fikri2701@gmail.com>
2024-01-31 14:55:51 +0000

    Showing the Posts List

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { PostsList } from "./features/posts/PostList";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PostsList />
+    </div>
+  );
 }
